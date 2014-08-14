a743cb978672828f4a1ea09dfec999bd936de0a1
eirslett <eirik.sletteberg@finn.no>
2014-08-14 10:39:17 +0200

    Remove solutions

--- src/main/java/introduction/AdvancedProblems.java
+++ src/main/java/introduction/AdvancedProblems.java
@@ -6,16 +6,12 @@ import java.util.stream.Collectors;
 public final class AdvancedProblems {
     // I en liste med tall, finn det høyeste tallet. (Bruk "reduce"-metoden)
     public static Long findHighestNumber(List<Long> numbers){
-        return numbers
-                .stream()
-                .reduce(numbers.get(0), Long::max);
+        return null;
     }
 
     // Finn summen av en liste med tall. (Bruk "reduce"-metoden)
     public static Long sum(List<Long> numbers){
-        return numbers
-                .stream()
-                .reduce(0L, Long::sum);
+        return null;
     }
 
     // Lag en timed-funksjon. Denne skal fungere sånn:
@@ -25,49 +21,35 @@ public final class AdvancedProblems {
     // timed("Gjøre ting som tar lang tid", () -> doStuffThatTakesALongTime());
     // Resultat: "Gjøre ting som tar lang tid: Det tok 1200 ms å kjøre."
     private static void timed(String description, Runnable task) {
-        long start = System.currentTimeMillis();
-        task.run();
-        long end = System.currentTimeMillis();
-        long duration = end - start;
-        System.out.println(description+": "+"Det tok "+duration+" ms å kjøre.");
+
     }
 
     // Returner liste med alle primtall (bruk "isPrime"-metoden til hjelp).
     // F.eks. (2, 5, 7, 9, 11, 13, 15) -> (2, 5, 7, 11, 13)
     // Du skal bruke timed-funksjonen som du lagde i stad.
     public static List<Integer> getPrimesOneByOne(List<Integer> numbers) {
-        return numbers
-                .stream()
-                .filter(i -> isPrime(i))
-                .collect(Collectors.toList());
+        return null;
     }
 
     // Returner liste med alle primtall, slik som i forrige oppgave.
     // Dette skal gjøres i parallell (bruk "parallelStream"-metoden), for at det skal gå raskere.
     public static List<Integer> getPrimesParallel(List<Integer> numbers) {
-        return numbers
-                .parallelStream()
-                .filter(i -> isPrime(i))
-                .collect(Collectors.toList());
+        return null;
     }
 
     // Bruk getPrimesOneByOne-metoden til å skrive ut alle partall fra ei liste.
     // Du skal bruke timed-metoden for å måle hvor lang tid det tok.
-    // (bruk beskrivelse "Primtall en og en")
+    // (bruk beskrivelse "Primtall en og en" i timeren)
     public static void printPrimesOneByOne(List<Integer> numbers) {
-        timed("Primtall en og en", () -> {
-            getPrimesOneByOne(numbers).forEach(System.out::println);
-        });
+
     }
 
     // Bruk getPrimesParallel-metoden til å skrive ut alle partall fra ei liste.
     // Du skal bruke timed-metoden for å måle hvor lang tid det tok.
     // Sjekk at det går raskere å kjøre i parallell!
-    // (bruk beskrivelse "Primtall i parallell")
+    // (bruk beskrivelse "Primtall i parallell" i timeren)
     public static void printPrimesParallel(List<Integer> numbers) {
-        timed("Primtall i parallell", () -> {
-            getPrimesParallel(numbers).forEach(System.out::println);
-        });
+
     }
 
     private static boolean isPrime(Integer n) {
--- src/main/java/introduction/IntroProblems.java
+++ src/main/java/introduction/IntroProblems.java
@@ -9,40 +9,31 @@ public class IntroProblems {
     // For hvert element i ei liste ("a", "b", "c"), skriv ut elementet.
     // Bruk "forEach"-metoden.
     public static void printElements(List<String> elements) {
-        elements.forEach(System.out::println);
+
     }
 
     // For hver person (Arne Skogen, Håkon Solstad, Halvard Knutsen), skriv ut fornavet i store bokstaver
     // Bruk bun "forEach"-metoden
     public static void printUppercaseFirstNames(List<Person> persons) {
-        persons.forEach(p -> System.out.println(p.firstName.toUpperCase()));
+
     }
 
     // For hver person (Arne Skogen, Håkon Solstad, Halvard Knutsen), skriv ut fornavet i store bokstaver
     // Bruk først "stream", så "map" for å finne fornavn i store bokstaver, og så "collect"-metoden for å gjøre om til liste.
     public static List<String> getUppercaseFirstNamesUsingMap(List<Person> persons) {
-        return persons
-                .stream()
-                .map(p -> p.firstName.toUpperCase())
-                .collect(Collectors.toList());
+        return null;
     }
 
     // Fra en liste med tall (1, 2, 3, 4, 5, 6) skal man finne alle partall (2, 4, 6) og returnere denne lista.
     // Bruk først "stream", så "filter" og til slutt "collect".
     public static List<Integer> findEvenNumbers(List<Integer> numbers){
-        return numbers
-                .stream()
-                .filter(n -> n % 2 == 0)
-                .collect(Collectors.toList());
+        return null;
     }
 
     // Fra en liste med tall skal man regne ut alle potenser (1, 2, 3) -> (1*1, 2*2, 3*3) = (1, 4, 9)
     // Denne lista skal så returneres.
     public static List<Integer> computeSquareOnAllNumbers(List<Integer> numbers){
-        return numbers
-                .stream()
-                .map(n -> n * n)
-                .collect(Collectors.toList());
+        return null;
     }
 
     // Man skal sortere personer etter etternavn.
@@ -51,22 +42,17 @@ public class IntroProblems {
     // må man lage en ny kopi av listen man skal sortere:
     // List<Person> listToSort = new ArrayList<>(persons);
     public static List<Person> sortByLastName(List<Person> persons){
-        List<Person> sortedList = new ArrayList<>(persons);
-        sortedList.sort((person1, person2) -> person1.lastName.compareTo(person2.lastName));
-        return sortedList;
+        return null;
     }
 
     // En mer generell versjon av filtrering: gitt et predikat, finn alle tall i lista som matcher dette predikatet.
     // (f.eks. hvis lista er (1, 2, 3, 4, 5) og predikatet er "skal være oddetall" så blir lista (1, 3, 5)
     public static List<Integer> findSublistFromPredicate(List<Integer> numbers, Predicate<Integer> predicate){
-        return numbers
-                .stream()
-                .filter(predicate)
-                .collect(Collectors.toList());
+        return null;
     }
 
     // Returner et predikat som skal matche hvis en string ikke er tom. (Anta at den aldri vil være null)
     public static Predicate<String> makeIsNotEmptyPredicate() {
-        return s -> s.length() > 0;
+        return null;
     }
 }
